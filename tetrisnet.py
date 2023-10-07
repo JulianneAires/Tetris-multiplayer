99209cbd368aa70287ce7c1e54c19bd7ae2567ca
JulianneAires <125161871+JulianneAires@users.noreply.github.com>
2023-10-07 17:14:14 -0300

    interface jogo

--- tetrisnet.py
+++ tetrisnet.py
@@ -11,11 +11,18 @@ clock = pygame.time.Clock()
 
 # Cores
 preto = (0, 0, 0)
-azul = (0, 0, 255)
-verde = (0, 255, 0)
-vermelho = (255, 0, 0)
 
- # Formatos
+cores_formas = {
+    0: (0, 255, 0),  # Barra (verde)
+    1: (255, 0, 0),  # Quadrado (vermelho)
+    2: (0, 255, 255),  # L (ciano)
+    3: (255, 255, 0),  # L invertido (amarelo)
+    4: (255, 165, 0),  # T (laranja)
+    5: (255, 0, 128),  # Z (rosa)
+    6: (128, 0, 255)  # Z invertido (cinza)
+}
+
+# Formatos
 formas = [
     [[1, 1, 1, 1]],  # Barra
     [[1, 1], [1, 1]],  # Quadrado
@@ -28,23 +35,109 @@ formas = [
 
 def criar_forma():
     return random.choice(formas)
- 
-def desenhar_forma(forma, x, y):
+
+def desenhar_forma(forma, x, y, cores_formas ):
     for linha in range(len(forma)):
         for coluna in range(len(forma[linha])):
             if forma[linha][coluna]:
-                pygame.draw.rect(tela, azul, (x + coluna * 30, y + linha * 30, 30, 30))
+                pygame.draw.rect(tela, cores_formas , (x + coluna * 30, y + linha * 30, 30, 30))
                 pygame.draw.rect(tela, preto, (x + coluna * 30, y + linha * 30, 30, 30), 2)
 
-while True: 
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.display.quit()
-                quit()
+def desenhar_grade(surface):
+    cor_grade = (128, 128, 128)
+    espacamento = 30
+    # Linhas horizontais
+    for linha in range(altura // espacamento):
+        pygame.draw.line(surface, cor_grade, (0, linha * espacamento), (largura, linha * espacamento))
 
-    pygame.display.update()
-    clock.tick(60)
+    # Linhas verticais
+    for coluna in range(largura // espacamento):
+        pygame.draw.line(surface, cor_grade, (coluna * espacamento, 0), (coluna * espacamento, altura))
+
+def colisao(forma, x, y, grid):
+    for linha in range(len(forma)):
+        for coluna in range(len(forma[linha])):
+            if forma[linha][coluna]:
+                pos_x = (x + coluna * 30) // 30
+                pos_y = (y + linha * 30) // 30
+
+                # Verifica se a posição está fora dos limites da tela
+                if (
+                    pos_x < 0
+                    or pos_x >= len(grid[0])
+                    or pos_y >= len(grid)
+                    or grid[pos_y][pos_x] != (0, 0, 0)
+                ):
+                    return True
+    return False
+
+def main():
+    global grid
+
+    grid = [[(0, 0, 0) for _ in range(10)] for _ in range(20)]
     x, y = largura // 2 - 15, 0
     forma_atual = criar_forma()
-    desenhar_forma(forma_atual, x, y)
+    cor_atual = cores_formas[formas.index(forma_atual)]
+    velocidade_queda = 2
+    tempo_queda = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            x -= 30
+            if colisao(forma_atual, x, y, grid):
+                x += 30
+        if keys[pygame.K_RIGHT]:
+            x += 30
+            if colisao(forma_atual, x, y, grid):
+                x -= 30
+        if keys[pygame.K_DOWN]:
+            y += 30
+            if colisao(forma_atual, x, y, grid):
+                y -= 30
+
+        if not colisao(forma_atual, x, y + 30, grid):
+            tempo_queda += 1
+            if tempo_queda >= velocidade_queda:
+                y += 30
+                tempo_queda = 0
+        else:
+            # Bloqueia o bloco caso ele tenha chegado ao fim 
+            for linha in range(len(forma_atual)):
+                for coluna in range(len(forma_atual[linha])):
+                    if forma_atual[linha][coluna]:
+                        grid[(y + linha * 30) // 30][(x + coluna * 30) // 30] = cor_atual
+
+            # Verifica se tem fileiras completas pra eliminar
+            linhas_completas = []
+            for linha in range(len(grid)):
+                if all(c != (0, 0, 0) for c in grid[linha]):
+                    linhas_completas.append(linha)
+            for linha_completa in linhas_completas:
+                del grid[linha_completa]
+                grid.insert(0, [(0, 0, 0) for _ in range(10)])
+
+            # Gere um novo bloco
+            forma_atual = criar_forma()
+            x, y = largura // 2 - 15, 0
+
+        tela.fill(preto)
+        desenhar_grade(tela)  # Desenhe a grade no fundo
+        for linha in range(len(grid)):
+            for coluna in range(len(grid[linha])):
+                if grid[linha][coluna] != (0, 0, 0):
+                    pygame.draw.rect(tela, grid[linha][coluna], (coluna * 30, linha * 30, 30, 30))
+                    pygame.draw.rect(tela, preto, (coluna * 30, linha * 30, 30, 30), 2)
+
+        desenhar_forma(forma_atual, x, y, cor_atual)
+
+        pygame.display.update()
+        clock.tick(5)
+
+if __name__ == "__main__":
+    main()
